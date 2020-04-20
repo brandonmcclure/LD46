ce0eb9482f2558d547f3f32e087227920ab230e8
Brandon McClure <brandonmcclure89@gmail.com>
2020-04-20 10:25:53 -0600

    a

--- src/index.php
+++ src/index.php
@@ -1,1 +1,6 @@
-<?php include("header.php"); 
+<?php include("header.php"); ?>
+<?php include("jumbotronHeader.php"); ?>
+<h1 >This is my entry to Ludam Dare 46.</h1>
+<p>Press the button below to start</p>
+<p><a class="btn btn-lg btn-success" href="ResetSession.php" role="button">Start Game</a></p>
+<?php include("footer.php"); ?>
